bfd04a26e7afe2dbbddf11ed95fb4c3460ea2d56
Crowdbotics <team@crowdbotics.com>
2023-07-28 15:54:20 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "white-moon-42486",
+  url: "https://white-moon-42486.botics.co",
+  api: "https://white-moon-42486.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
